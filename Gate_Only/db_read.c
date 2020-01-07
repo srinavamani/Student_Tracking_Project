42614123cb1735d6b2b4bc3e7ef1dab53dc31f9c
Sri Navamani <srinavamani1992@gmail.com>
2020-01-08 02:31:26 +0530

    Added First level code - Demo release

--- Gate_Only/db_read.c
+++ Gate_Only/db_read.c
@@ -15,11 +15,6 @@
 
 int callback(void *NotUsed, int argc, char **argv, char **azColName);
 
-int state = 0;
-int number_of_channel = 0;
-char entry_logic_IN[25];
-char entry_logic_OUT[25];
-
 int main(int argc, char *argv[])
 {
 printf("argc = %d\n",argc);
@@ -27,15 +22,20 @@ printf("argc = %d\n",argc);
 if(argc==5)
 {
 if(strcmp(argv[1],MORNING)==0)
-section=1;
+	section=1;
 else if(strcmp(argv[1],AFTERNOON)==0)
-section=2;
+	section=2;
+else
+{
+	printf("Wrong Arguments\n");
+	return 0;
+}
 
 printf("section - %d\n",section);
 
 sscanf(argv[2], "%d", &number_of_channel);
 printf("Number of channel = %d\n",number_of_channel);
-state = number_of_channel;
+
 //IN Logic
 strcpy(entry_logic_IN,argv[3]);
 printf("IN Logic = %s\n",entry_logic_IN);
@@ -44,9 +44,7 @@ printf("IN Logic = %s\n",entry_logic_IN);
 strcpy(entry_logic_OUT,argv[4]);
 printf("OUT Logic = %s\n",entry_logic_OUT);
 
-
 // Coding starts from here,
-
 system("rm /home/root/ID.db");
 
 csv_access();
@@ -57,7 +55,6 @@ if(db_init() == 0)
 csv_db();
 printf("CSV converted into db file\n");
 check_timing();
-
 }
 else
 {
